8adabaed640ea73a0e72b5c2bbd59a9a3f5ff3ee
idning <idning@gmail.com>
2014-06-29 20:45:18 +0800

    kv.c

---
+++ src/kv.c
@@ -0,0 +1,23 @@
+/*
+ * file   : example.c
+ * author : ning
+ * date   : 2014-06-28 08:34:37
+ */
+
+#include <stdio.h>
+
+typedef struct kv_server_s {
+    /* General */
+    char *config_file;          /* Absolute config file path, or NULL */
+
+}kv_server_t;
+
+
+int main(int argc, const char **argv) {
+
+    return 0;
+}
+
+/* vim: set expandtab ts=4 sw=4 sts=4 tw=100: */
+
+
